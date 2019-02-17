9bf40a2fde9f0be9437cc2ff368d0e9068efb369
Jeewhan <distinct19@gmail.com>
2019-02-17 17:34:45 +0900

    react-router-dom 설치 및 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 
 import { Helmet } from "react-helmet";
+import { Route, Switch } from "react-router-dom";
+
+import { Home } from "pages";
 
 const App = () => (
-  <Helmet>
-    <title>모이다</title>
-    <meta
-      name="description"
-      content="다른 사람들과 공유하고 싶을 때, 모이다에서 모여요."
-    />
-  </Helmet>
+  <>
+    <Helmet>
+      <title>모이다</title>
+      <meta
+        name="description"
+        content="다른 사람들과 공유하고 싶을 때, 모이다에서 모여요."
+      />
+    </Helmet>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </>
 );
 
 export default App;
