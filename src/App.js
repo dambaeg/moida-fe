4071351f6bca615887d05bc22854357735d52228
Jeewhan <distinct19@gmail.com>
2019-02-17 15:13:45 +0900

    react-helmet 설치 및 반영

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Helmet } from "react-helmet";
+
+const App = () => (
+  <Helmet>
+    <title>모이다</title>
+    <meta
+      name="description"
+      content="다른 사람들과 공유하고 싶을 때, 모이다에서 모여요."
+    />
+  </Helmet>
+);
+
+export default App;
